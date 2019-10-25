e260ee92a1203687cb4a7bf5b95a33e9263a5b96
Savinov Aleksandr <a.savinov@plarium.Local>
2019-10-25 16:18:21 +0300

    Multiple Scenes 1-2

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,41 +1,51 @@
-﻿
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using DefaultNamespace;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
 
 public class GameController : PersistableObject
 {
     const int saveVersion = 3;
-    
+
     public ShapeFactory ShapeFactory;
     public float CreationSpeed { get; set; }
     public float DestructionSpeed { get; set; }
 
-    [SerializeField] 
-    private PersistentStorage _storage;
+    [SerializeField] private PersistentStorage _storage;
 
-    [SerializeField] 
-    private KeyCode _spawnKey;
-    [SerializeField] 
-    private KeyCode _destroyKey = KeyCode.X;
-    [SerializeField] 
-    private KeyCode _newGameKey;
-    [SerializeField] 
-    private KeyCode _saveKey = KeyCode.S;
-    [SerializeField] 
-    private KeyCode _loadKey = KeyCode.L;
+    [SerializeField] private KeyCode _spawnKey;
+    [SerializeField] private KeyCode _destroyKey = KeyCode.X;
+    [SerializeField] private KeyCode _newGameKey;
+    [SerializeField] private KeyCode _saveKey = KeyCode.S;
+    [SerializeField] private KeyCode _loadKey = KeyCode.L;
 
     private List<Shape> _shapes;
     private float _creationProgress, _destructionProgress;
-    
+
 
     private void Awake()
     {
         _creationProgress = 0f;
         _shapes = new List<Shape>();
     }
-    
+
+    private void Start()
+    {
+#if UNITY_EDITOR
+        Scene loadedLevel = SceneManager.GetSceneByName("Level1");
+        if (loadedLevel.isLoaded)
+        {
+            SceneManager.SetActiveScene(loadedLevel);
+            return;
+        }
+#endif
+
+        StartCoroutine(LoadLevel());
+    }
+
     void Update()
     {
         _creationProgress += Time.deltaTime * CreationSpeed;
@@ -44,14 +54,14 @@ public class GameController : PersistableObject
             _creationProgress -= 1f;
             SpawnShape();
         }
-        
+
         _destructionProgress += Time.deltaTime * DestructionSpeed;
         while (_destructionProgress >= 1f)
         {
             _destructionProgress -= 1f;
             DestroyShape();
         }
-        
+
         if (Input.GetKey(_spawnKey))
         {
             SpawnShape();
@@ -61,7 +71,7 @@ public class GameController : PersistableObject
         {
             DestroyShape();
         }
-        
+
         if (Input.GetKeyDown(_newGameKey))
         {
             NewGame();
@@ -74,6 +84,15 @@ public class GameController : PersistableObject
         {
             _storage.Load(this);
         }
+    }
+
+    private IEnumerator LoadLevel()
+    {
+        enabled = false;
+        yield return
+            SceneManager.LoadSceneAsync("Level1", LoadSceneMode.Additive);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("Level1"));
+        enabled = true;
     }
 
     private void SpawnShape()
@@ -90,9 +109,11 @@ public class GameController : PersistableObject
         ));
         _shapes.Add(instance);
     }
-    
-    private void DestroyShape () {
-        if (_shapes.Count > 0) {
+
+    private void DestroyShape()
+    {
+        if (_shapes.Count > 0)
+        {
             int index = Random.Range(0, _shapes.Count);
             ShapeFactory.Reclaim(_shapes[index]);
             int lastIndex = _shapes.Count - 1;
@@ -107,27 +128,31 @@ public class GameController : PersistableObject
         {
             ShapeFactory.Reclaim(instance);
         }
-        
+
         _shapes.Clear();
     }
-    
-    public override void Save (GameDataWriter writer) {
+
+    public override void Save(GameDataWriter writer)
+    {
         writer.Write(_shapes.Count);
-        for (int i = 0; i < _shapes.Count; i++) {
+        for (int i = 0; i < _shapes.Count; i++)
+        {
             writer.Write(_shapes[i].ShapeId);
             writer.Write(_shapes[i].MaterialId);
             _shapes[i].Save(writer);
         }
     }
-    
-    public override void Load (GameDataReader reader)
+
+    public override void Load(GameDataReader reader)
     {
         int version = reader.Version;
         int count = version <= 0 ? -version : reader.ReadInt();
-        if (version > saveVersion) {
+        if (version > saveVersion)
+        {
             Debug.LogError("Unsupported future save version " + version);
             return;
         }
+
         for (int i = 0; i < count; i++)
         {
             var shapeId = version > 0 ? reader.ReadInt() : 0;
--- Assets/Scripts/ShapeFactory.cs
+++ Assets/Scripts/ShapeFactory.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
 
 namespace DefaultNamespace
@@ -8,48 +10,71 @@ namespace DefaultNamespace
     [CreateAssetMenu]
     public class ShapeFactory : ScriptableObject
     {
-        [SerializeField]
-        private Shape[] _shapes;
-        
-        [SerializeField]
-        private Material[] _materials;
+        [SerializeField] private Shape[] _shapes;
 
-        [SerializeField] 
-        private bool _recycle;
-        
-        [NonSerialized]
-        private List<Shape>[] _pools;
-        
-        void CreatePools () {
+        [SerializeField] private Material[] _materials;
+
+        [SerializeField] private bool _recycle = false;
+
+        [NonSerialized] private List<Shape>[] _pools;
+
+        [NonSerialized] private Scene _poolScene;
+
+        void CreatePools()
+        {
             _pools = new List<Shape>[_shapes.Length];
-            for (int i = 0; i < _pools.Length; i++) {
+            for (int i = 0; i < _pools.Length; i++)
+            {
                 _pools[i] = new List<Shape>();
             }
+
+#if UNITY_EDITOR
+            _poolScene = SceneManager.GetSceneByName(name);
+            if (_poolScene.isLoaded)
+            {
+                var inactiveShapes = _poolScene
+                    .GetRootGameObjects()
+                    .Where(go => !go.activeSelf)
+                    .Select(go => go.GetComponent<Shape>());
+                foreach (var shape in inactiveShapes)
+                {
+                    _pools[shape.ShapeId].Add(shape);
+                }
+                return;
+            }
+#endif
+
+            _poolScene = SceneManager.CreateScene(name);
         }
-        
-        
-        public Shape Get (int shapeId = 0, int materialId = 0)
+
+
+        public Shape Get(int shapeId = 0, int materialId = 0)
         {
             Shape instance;
-            
-            if (_recycle) {
-                if (_pools == null) {
+
+            if (_recycle)
+            {
+                if (_pools == null)
+                {
                     CreatePools();
                 }
-                
+
                 List<Shape> pool = _pools[shapeId];
                 int lastIndex = pool.Count - 1;
                 
-
-                if (lastIndex >= 0) {
+                
+                if (lastIndex >= 0)
+                {
                     instance = pool[lastIndex];
                     pool.RemoveAt(lastIndex);
                 }
-                else {
+                else
+                {
                     instance = Instantiate(_shapes[shapeId]);
                     instance.ShapeId = shapeId;
+                    SceneManager.MoveGameObjectToScene(instance.gameObject, _poolScene);
                 }
-                
+
                 instance.gameObject.SetActive(true);
             }
             else
@@ -57,24 +82,29 @@ namespace DefaultNamespace
                 instance = Instantiate(_shapes[shapeId]);
                 instance.ShapeId = shapeId;
             }
-            
-            
+
             instance.SetMaterial(_materials[materialId], materialId);
+
             return instance;
         }
-        
-        public Shape GetRandom () {
+
+        public Shape GetRandom()
+        {
             return Get(
-                Random.Range(0, _shapes.Length), 
+                Random.Range(0, _shapes.Length),
                 Random.Range(0, _materials.Length)
-                );
+            );
         }
-        
-        public void Reclaim (Shape shapeToRecycle) {
-            if (_recycle) {
-                if (_pools == null) {
+
+        public void Reclaim(Shape shapeToRecycle)
+        {
+            if (_recycle)
+            {
+                if (_pools == null)
+                {
                     CreatePools();
                 }
+
                 _pools[shapeToRecycle.ShapeId].Add(shapeToRecycle);
                 shapeToRecycle.gameObject.SetActive(false);
             }
