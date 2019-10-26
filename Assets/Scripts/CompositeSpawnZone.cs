e2457d869ea82cd467ed41eaee4756845e485c88
Savinov Aleksandr <a.savinov@plarium.Local>
2019-10-26 16:00:41 +0300

    in progress

--- Assets/Scripts/CompositeSpawnZone.cs
+++ Assets/Scripts/CompositeSpawnZone.cs
@@ -6,13 +6,26 @@ namespace DefaultNamespace
 {
     public class CompositeSpawnZone : SpawnZone
     {
+        [SerializeField] private bool _sequential;
         [SerializeField] private SpawnZone[] _spawnZones;
 
+        [NonSerialized] private int _nextSequentialIndex;
+        
         public override Vector3 SpawnPoint
         {
             get
             {
-                int zoneIndex = Random.Range(0, _spawnZones.Length);
+                int zoneIndex;
+
+                if (_sequential)
+                {
+                    zoneIndex = _nextSequentialIndex++;
+                    _nextSequentialIndex = _nextSequentialIndex % _spawnZones.Length;
+                }
+                else
+                {
+                    zoneIndex = Random.Range(0, _spawnZones.Length);
+                }
 
                 return _spawnZones[zoneIndex].SpawnPoint;
             }
